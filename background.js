50f57fa4c1f44a7e52cdee42d48171c37980d6ae
Daniel Lecklider <lecklider.6@gmail.com>
2017-10-23 21:40:29 -0400

    Working version

--- background.js
+++ background.js
@@ -14,14 +14,24 @@
   
 // }
  
- function eventFire(el, etype){
-  if (el.fireEvent) {
-    el.fireEvent('on' + etype);
-  } else {
-    var evObj = document.createEvent('Events');
-    evObj.initEvent(etype, true, false);
-    el.dispatchEvent(evObj);
-  }
-}
+var dispatchMouseEvent = function(target, var_args) {
+  var e = document.createEvent("MouseEvents");
+  // If you need clientX, clientY, etc., you can call
+  // initMouseEvent instead of initEvent
+  e.initEvent.apply(e, Array.prototype.slice.call(arguments, 1));
+  target.dispatchEvent(e);
+};
 
-eventFire(document.getElementById(':a'), 'click');
+var menu = document.querySelector("[data-id=now-playing-menu]");
+
+dispatchMouseEvent(menu, 'mouseover', true, true);
+dispatchMouseEvent(menu, 'mousedown', true, true);
+dispatchMouseEvent(menu, 'click', true, true);
+dispatchMouseEvent(menu, 'mouseup', true, true);
+
+var element = document.getElementById(":a");
+
+dispatchMouseEvent(element, 'mouseover', true, true);
+dispatchMouseEvent(element, 'mousedown', true, true);
+dispatchMouseEvent(element, 'click', true, true);
+dispatchMouseEvent(element, 'mouseup', true, true);
